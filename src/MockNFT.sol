92e84391aff9603a52c36504edabbfbe6a472c5d
odigome <odigome1995@gmail.com>
2026-06-02 16:28:45 +0100

    Initial Foundry setup with MockNFT

---
+++ src/MockNFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockNFT is ERC721, Ownable {
+    uint256 private _nextTokenId;
+
+constructor() ERC721("Mock NFT", "MNFT") Ownable(msg.sender) {}
+
+    function mint(address to) external returns (uint256) {
+        uint256 tokenId = _nextTokenId;
+        _safeMint(to, tokenId);
+        _nextTokenId++;
+        return tokenId;
+    }
+    function mintTo(address to) external onlyOwner returns (uint256) {
+        uint256 tokenId = _nextTokenId;
+        _safeMint(to, tokenId);
+        _nextTokenId++;
+        return tokenId;
+    }
+    function nextTokenId() external view returns (uint256) {
+        return _nextTokenId;
+    }
+} 
