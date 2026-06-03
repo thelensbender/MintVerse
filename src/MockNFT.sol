97bc6df6d19b8c2f54307790e79735d9bbf98cca
solomonadebayo3212 <YOUR_GITHUB_EMAIL>
2026-06-02 17:33:27 -0700

    fix: MockNFT mint accepts explicit tokenId

--- src/MockNFT.sol
+++ src/MockNFT.sol
@@ -4,13 +4,9 @@ pragma solidity ^0.8.24;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockNFT is ERC721 {
-    uint256 private _nextTokenId;
-
     constructor() ERC721("MockNFT", "MNFT") {}
 
-    function mint(address to) external returns (uint256) {
-        uint256 tokenId = ++_nextTokenId;
+    function mint(address to, uint256 tokenId) external {
         _mint(to, tokenId);
-        return tokenId;
     }
 }
