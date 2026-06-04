2aa0fc9a99b1449377e5dfd765d9b9929aab97ba
odigome <odigome1995@gmail.com>
2026-06-04 15:11:58 +0100

    fix formatting

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -11,7 +11,8 @@ import "../src/NFTMarketplace.sol";
 contract DeployScript is Script {
     function run() external {
         // 3. Tell the robot to look inside your computer wallet for authorization
-        uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
+        uint256 deployerPrivateKey =
+            vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
 
         // 4. Start the official deployment transmission
         vm.startBroadcast(deployerPrivateKey);
